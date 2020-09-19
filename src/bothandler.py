10abfee82dc8efd64ecc1a9921ee279c3367f4c5
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2020-09-19 18:00:17 +0200

    Added venue and contact information

--- src/bothandler.py
+++ src/bothandler.py
@@ -69,57 +69,72 @@ def handleUpdate(update_dict):
     emojiPoultryLeg = u'\U0001F357'
     emojiBacon = u'\U0001F953'
     emojiPig = u'\U0001F416'
+    emojiRobot = u'\U0001F916'
 
     # Commands
     commandPizza = "/pizza"
     commandStart = "/start"
     commandRandom = "/zufall"
+    commandAddress = "/kontakt"
+    commandHelp = "/help"
 
     numberCells = 3
+
+    # Kantine data
+    # d49.9936668!4d8.4121598
+    KantineLat = 49.9936668
+    KantineLong = 8.4121598
+    KantineTitle = "Die Holzofen Kantine"
+    KantineAddress = "Marktstraße 23, 65428 Rüsselsheim am Main"
+    KantineNumber = "+496142795232"
+
+    # API related vars
     methodMsg = "sendMessage"
+    methodContact = "sendContact"
+    methodVenue = "sendVenue"
     parseMode = "MarkdownV2"
 
     # Pre formatted strings
     TextSelectToppings = "Bitte Beläge auswählen und dann mit Button bestätigen"
     TextNichtVegetarisch = "\n{} _Nicht vegetarisch_".format(emojiPig)
     TextVegetarisch = "\n{} _Vegetarisch_".format(emojiSeedling)
-    TextStart = """Hi, ich bins\\! Der KantineBot {emojiSweatSmile}
+    TextStart = """Hi, ich bins\\! Der *KantineBot* {emojiRobot}{emojiSweatSmile}
+_by \\@das\\_kaesebrot_
 
 Folgende Kommandos sind bei mir verfügbar:
 
 {emojiPizza} /pizza \\- Die 3 günstigsten Pizzen nach Auswahl deiner Beläge
 {emojiDie} /zufall \\- Lass dir vom Bot eine zufällige Pizza vorschlagen\\!
 
-{emojiPin} /addresse \\- Daten der Holzofen\\-Kantine
+{emojiPin} /kontakt \\- Daten der Holzofen\\-Kantine
 
-Guten Appetit\\! {emojiPizza}
-""".format(emojiSweatSmile = emojiSweatSmile, emojiPizza = emojiPizza, emojiDie = emojiDie, emojiPin = emojiPin)
+{emojiRobot} /help \\- Informationen zum Bot
+
+Guten Appetit\\! {emojiPizza}""".format(emojiSweatSmile = emojiSweatSmile, emojiPizza = emojiPizza, emojiDie = emojiDie, emojiPin = emojiPin, emojiRobot = emojiRobot)
 
 
     TextPizza = """*Nummer {nummer}*
 *{name}* mit {zutaten}
-*Preis:* {preis}{vegetarisch}
-"""
+*Preis:* {preis}{vegetarisch}"""
 
 
-    TextEnd = """Guten Appetit\\! {emojiFaceSavouringFood}{emojiForkKnife}
-""".format(emojiFaceSavouringFood = emojiFaceSavouringFood, emojiForkKnife = emojiForkKnife)
+    TextEnd = """Guten Appetit\\! {emojiFaceSavouringFood}{emojiForkKnife}""".format(emojiFaceSavouringFood = emojiFaceSavouringFood, emojiForkKnife = emojiForkKnife)
 
     TextGeneric = """{emojiPizza} Hi {{firstName}}, hier sind deine Pizzen:\n
 {{TextPizza1}}\n
 {{TextPizza2}}\n
-{{TextPizza3}}
-{TextEnd}
-""".format(emojiPizza = emojiPizza, TextEnd = TextEnd)
+{{TextPizza3}}\n
+{TextEnd}""".format(emojiPizza = emojiPizza, TextEnd = TextEnd)
 
     TextRandom = """{emojiDie} Hi {{firstName}}, hier ist deine Zufallspizza:\n
-{{TextPizza}}
-{TextEnd}
-""".format(emojiDie = emojiDie, TextEnd = TextEnd)
+{{TextPizza}}\n
+{TextEnd}""".format(emojiDie = emojiDie, TextEnd = TextEnd)
+
+    TextAddress = """{emojiPin} Hier die Daten:""".format(emojiPin = emojiPin)
 
     for update in update_dict["result"]:
         if "message" in update.keys():
-            if (update["message"]["text"] == commandStart):
+            if (update["message"]["text"] == commandStart) or (update["message"]["text"] == commandHelp):
                 params = {}
                 params["chat_id"] = update["message"]["from"]["id"]
                 params["text"] = TextStart
@@ -195,6 +210,28 @@ Guten Appetit\\! {emojiPizza}
                 params["text"] = TextRandomFull
                 params["parse_mode"] = parseMode
                 print(apiCall(reqPath, methodMsg, params).text)
+
+            elif (update["message"]["text"] == commandAddress):
+                params = {}
+                params["chat_id"] = update["message"]["from"]["id"]
+                params["text"] = TextAddress
+                params["parse_mode"] = parseMode
+                print(apiCall(reqPath, methodMsg, params).text)
+
+                params = {}
+                params["chat_id"] = update["message"]["from"]["id"]
+                params["latitude"] = KantineLat
+                params["longitude"] = KantineLong
+                params["title"] = KantineTitle
+                params["address"] = KantineAddress
+                print(apiCall(reqPath, methodVenue, params).text)
+
+                params = {}
+                params["chat_id"] = update["message"]["from"]["id"]
+                params["phone_number"] = KantineNumber
+                params["first_name"] = KantineTitle
+                print(apiCall(reqPath, methodContact, params).text)
+
 
 def str2bool(string_, default='raise'):
     true = ['true', 't', '1', 'y', 'yes', 'enabled', 'enable', 'on']
--- src/confighandler.py
+++ src/confighandler.py
@@ -29,4 +29,3 @@ def readConfig():
 
 def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
-
