ce10ac5130b0e00b2566e986c7f1f504450027d2
das-kaesebrot <das-kaesebrot@users.noreply.github.com>
2020-09-21 20:02:22 +0200

    Added exception handling

--- kantine-bot.py
+++ kantine-bot.py
@@ -1,19 +1,27 @@
 #! /usr/bin/env python3
 from pathlib import Path
 import os
+import sys
 from src import pizza
 from src import confighandler
 from src import bothandler
 
 if __name__ == "__main__":
-    assetsfolder = "assets/csv"
-    pizzaFile = "kantine.csv"
-    extrasFile = "extras.csv"
+    try:
+        assetsfolder = "assets/csv"
+        pizzaFile = "kantine.csv"
+        extrasFile = "extras.csv"
 
-    pizzaPath = os.path.join(confighandler.get_script_path(),assetsfolder,pizzaFile)
-    extrasPath = os.path.join(confighandler.get_script_path(),assetsfolder,extrasFile)
+        pizzaPath = os.path.join(confighandler.get_script_path(),assetsfolder,pizzaFile)
+        extrasPath = os.path.join(confighandler.get_script_path(),assetsfolder,extrasFile)
 
-    verbose = True
-    config = confighandler.readConfig()
+        verbose = True
+        config = confighandler.readConfig()
 
-    bothandler.updatePoller(config, verbose, pizza.makeFullDict(pizzaPath, extrasPath))
+        bothandler.updatePoller(config, verbose, pizza.makeFullDict(pizzaPath, extrasPath))
+        
+    except KeyboardInterrupt:
+        print("\n[{}] [OK] Program stopped".format(bothandler.getTime()))
+        sys.exit(0)
+    except Exception as e:
+        print("\n[{}] [ERROR] {}".format(bothandler.getTime(), e))
