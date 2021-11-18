a73d753e6c4383efa1fe1c00adc848dd6746fe69
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2021-11-18 04:49:45 +0100

    add webhook base url env variable

--- src/envhandler.py
+++ src/envhandler.py
@@ -11,8 +11,11 @@ def readEnv():
     env_vars = {}
     env_vars["BASEURL"] = os.getenv("API_BASEURL", "https://api.telegram.org/bot")
     env_vars["TOKEN"] = os.getenv("TGBOTTOKEN")
+    env_vars["HOOKSBASEURL"] = os.getenv("HOOKSBASEURL")
     
     if (env_vars["TOKEN"] == None) or (env_vars["TOKEN"] == ""):
         raise Exception("Bot token variable has not been set. Please set the environment variable TGBOTTOKEN and restart.")
-
+    if (env_vars["HOOKSBASEURL"] == None) or (env_vars["HOOKSBASEURL"] == ""):
+        raise Exception("Hooks base URL variable has not been set. Please set the environment variable HOOKSBASEURL and restart.")
+    
     return env_vars
