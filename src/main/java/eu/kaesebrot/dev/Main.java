6dd6bc07e5ff2d2b2918651d48fcfa38a14ed5b0
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-10 18:13:58 +0100

    [FEATURE] init springboot and bot in main

--- src/main/java/eu/kaesebrot/dev/Main.java
+++ src/main/java/eu/kaesebrot/dev/Main.java
@@ -1,7 +1,19 @@
 package eu.kaesebrot.dev;
 
+import eu.kaesebrot.dev.bot.PizzaSuggesterBot;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new PizzaSuggesterBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
