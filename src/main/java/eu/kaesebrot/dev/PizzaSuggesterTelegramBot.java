13e9a7a86b90b7bbbe7ed7a37d5da88e4a5e7749
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-10 22:58:28 +0100

    [FEATURE] disable spring boot startup banner

--- src/main/java/eu/kaesebrot/dev/PizzaSuggesterTelegramBot.java
+++ src/main/java/eu/kaesebrot/dev/PizzaSuggesterTelegramBot.java
@@ -1,5 +1,6 @@
 package eu.kaesebrot.dev;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PizzaSuggesterTelegramBot {
 
     public static void main(String[] args) {
-        SpringApplication.run(PizzaSuggesterTelegramBot.class, args);
+        SpringApplication app = new SpringApplication(PizzaSuggesterTelegramBot.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 }
