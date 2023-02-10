eefc709b4aea34eee52d14905c0b74cb3646438b
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-10 22:08:01 +0100

    [FEATURE] return BotApiMethod instance in handler

--- src/main/java/eu/kaesebrot/dev/handler/IUpdateHandler.java
+++ src/main/java/eu/kaesebrot/dev/handler/IUpdateHandler.java
@@ -1,7 +1,8 @@
 package eu.kaesebrot.dev.handler;
 
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public interface IUpdateHandler {
-    void handleUpdate(Update update);
+    BotApiMethod handleUpdate(Update update);
 }
