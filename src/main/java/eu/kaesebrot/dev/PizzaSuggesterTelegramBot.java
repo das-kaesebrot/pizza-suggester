75d74bdea8e2a75692e2898e2e12d75382f0a233
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-10 22:06:16 +0100

    [FEATURE] rename main method

---
+++ src/main/java/eu/kaesebrot/dev/PizzaSuggesterTelegramBot.java
@@ -0,0 +1,12 @@
+package eu.kaesebrot.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaSuggesterTelegramBot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaSuggesterTelegramBot.class, args);
+    }
+}
