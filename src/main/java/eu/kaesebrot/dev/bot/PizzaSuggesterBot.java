d0b0d132877a98609ffce534ed7099fc0c33880d
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-10 22:09:21 +0100

    [FEATURE] initial spring boot based telegram webhooks bot

--- src/main/java/eu/kaesebrot/dev/bot/PizzaSuggesterBot.java
+++ src/main/java/eu/kaesebrot/dev/bot/PizzaSuggesterBot.java
@@ -6,35 +6,42 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.starter.SpringWebhookBot;
 
 @Component
-public class PizzaSuggesterBot extends TelegramLongPollingBot {
+public class PizzaSuggesterBot extends SpringWebhookBot {
+    @Value("${telegrambot.token}")
+    private String botToken;
     @Autowired
     private IUpdateHandler updateHandler;
 
     Logger logger = LoggerFactory.getLogger(PizzaSuggesterBot.class);
 
-    @Value("${telegrambot.username}")
-    private String tgBotUsername;
-
-    @Value("${telegrambot.token}")
-    private String tgBotToken;
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        logger.debug("Got update: " + update.toString());
-        updateHandler.handleUpdate(update);
+    public PizzaSuggesterBot(SetWebhook setWebhook, String botToken) {
+        super(setWebhook, botToken);
     }
 
     @Override
     public String getBotUsername() {
-        return tgBotUsername;
+        return "KantineBot";
     }
 
     @Override
     public String getBotToken() {
-        return tgBotToken;
+        return botToken;
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        logger.debug("Got update: {}", update.toString());
+        return updateHandler.handleUpdate(update);
+    }
+
+    @Override
+    public String getBotPath() {
+        return null;
     }
 }
