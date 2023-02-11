e4ca88a0ce231b5742fb08fba90a7b4eebf6625e
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-11 21:36:15 +0100

    [FEATURE] admin key class

---
+++ src/main/java/eu/kaesebrot/dev/model/AdminKey.java
@@ -0,0 +1,44 @@
+package eu.kaesebrot.dev.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class AdminKey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID key;
+
+    @OneToOne
+    private CachedUser claimant;
+
+    public AdminKey() {
+    }
+
+    public UUID getKey() {
+        return key;
+    }
+
+    public CachedUser getClaimant() {
+        return claimant;
+    }
+
+    public boolean hasBeenClaimed() {
+        return !(claimant == null);
+    }
+
+    public void setClaimedBy(CachedUser claimant) {
+        if (hasBeenClaimed()) {
+            throw new RuntimeException("Admin key has already been claimed");
+        }
+
+        this.claimant = claimant;
+        this.claimant.setAdminStatus(true);
+    }
+
+    @Override
+    public String toString() {
+        return "AdminKey{" + key + "}";
+    }
+}
