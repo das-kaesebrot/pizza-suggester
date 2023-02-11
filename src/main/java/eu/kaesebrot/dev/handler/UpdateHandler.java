c7e456576cce89be2c53910c7157df1a19957695
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-11 02:28:25 +0100

    [FEATURE] implement inline keyboard generator

--- src/main/java/eu/kaesebrot/dev/handler/UpdateHandler.java
+++ src/main/java/eu/kaesebrot/dev/handler/UpdateHandler.java
@@ -6,10 +6,21 @@ import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.*;
 
 @Service
 public class UpdateHandler implements IUpdateHandler {
     Logger logger = LoggerFactory.getLogger(UpdateHandler.class);
+
+    // TODO replace with actual data
+    private List<String> ingredientListDummy;
+
+    private List<List<List<InlineKeyboardButton>>> inlineKeyboardPages;
+    private final int maxIngredientKeyboardRows = 3;
+    private final int maxIngredientKeyboardColumns = 2;
 
     public enum BotCommand {
         PIZZA,
@@ -86,5 +97,81 @@ public class UpdateHandler implements IUpdateHandler {
             default:
                 return BotCommand.UNKNOWN;
         }
+    }
+
+    private InlineKeyboardMarkup getInitialKeyboard() {
+        var keyboard = new InlineKeyboardMarkup();
+
+        getKeyboardForPage(null);
+
+        return keyboard;
+    }
+
+    private InlineKeyboardMarkup getKeyboardForPage(Optional<Integer> pageNumber) {
+        return new InlineKeyboardMarkup(getKeyboardButtonsForPage(pageNumber));
+    }
+
+    private List<List<InlineKeyboardButton>> getKeyboardButtonsForPage(Optional<Integer> pageNumber) {
+        if (!inlineKeyboardPages.isEmpty()) {
+            regenerateInlineKeyboardPageCache();
+        }
+
+        return inlineKeyboardPages.get(pageNumber.orElse(0));
+    }
+
+    private void regenerateInlineKeyboardPageCache() {
+        // clear cache whenever this method is called
+        inlineKeyboardPages = new ArrayList<>();
+
+        int pagesNeeded = ingredientListDummy.size() / (maxIngredientKeyboardColumns * maxIngredientKeyboardRows);
+
+        if (ingredientListDummy.size() % (maxIngredientKeyboardColumns * maxIngredientKeyboardRows) != 0) {
+            pagesNeeded++;
+        }
+
+        for (int page = 0; page < pagesNeeded; page++) {
+            var pageRows = new ArrayList<List<InlineKeyboardButton>>();
+
+            for (int rowCounter = 0; rowCounter < maxIngredientKeyboardRows; rowCounter++) {
+                var row = new ArrayList<InlineKeyboardButton>();
+
+                for (int columnCounter = 0; columnCounter < maxIngredientKeyboardColumns; columnCounter++) {
+                    var ingredientIndex = getIngredientIndexAtPageColumnAndRow(page, columnCounter, rowCounter);
+                    var button = new InlineKeyboardButton(ingredientListDummy.get(ingredientIndex));
+                    button.setCallbackData(String.valueOf(ingredientIndex));
+
+                    row.add(button);
+                }
+
+                pageRows.add(row);
+            }
+
+            pageRows.add(getLastRow(page));
+
+            inlineKeyboardPages.add(pageRows);
+        }
+    }
+
+    private List<InlineKeyboardButton> getLastRow(int pageNumber) {
+        // make sure the page number is 1-based instead of 0-based
+        pageNumber++;
+
+        var buttonLeft = new InlineKeyboardButton("\u2190");
+        buttonLeft.setCallbackData("previous");
+        var buttonRight = new InlineKeyboardButton("\u2190");
+        buttonRight.setCallbackData("next");
+        var buttonPageInfo = new InlineKeyboardButton("Seite " + pageNumber);
+        buttonLeft.setCallbackData("invalid");
+        var buttonConfirm = new InlineKeyboardButton("\u2713");
+        buttonRight.setCallbackData("confirm");
+
+        return List.of(buttonLeft, buttonRight, buttonPageInfo, buttonConfirm);
+    }
+
+    private int getIngredientIndexAtPageColumnAndRow(int pageNumber, int column, int row) {
+        int pageOffset = maxIngredientKeyboardColumns * maxIngredientKeyboardRows * pageNumber;
+        int indexWithOffset = column + row + pageOffset;
+
+        return indexWithOffset;
     }
 }
