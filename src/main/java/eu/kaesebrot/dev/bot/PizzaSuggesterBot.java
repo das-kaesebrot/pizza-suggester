183f82530a8113220478627f2b367f4af387b594
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-02-13 23:30:10 +0100

    [FEATURE] move update handling to instance of telegram bot

--- src/main/java/eu/kaesebrot/dev/bot/PizzaSuggesterBot.java
+++ src/main/java/eu/kaesebrot/dev/bot/PizzaSuggesterBot.java
@@ -1,28 +1,51 @@
 package eu.kaesebrot.dev.bot;
 
-import eu.kaesebrot.dev.handler.UpdateHandler;
 import eu.kaesebrot.dev.properties.TelegramBotProperties;
+import eu.kaesebrot.dev.service.AdminService;
+import eu.kaesebrot.dev.service.CachedUserRepository;
+import eu.kaesebrot.dev.service.IngredientInlineKeyboardService;
+import eu.kaesebrot.dev.service.VenueRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.starter.SpringWebhookBot;
 
 @Component
 public class PizzaSuggesterBot extends SpringWebhookBot {
-    @Autowired
-    private UpdateHandler updateHandler;
-
+    Logger logger = LoggerFactory.getLogger(PizzaSuggesterBot.class);
+    private final CachedUserRepository cachedUserRepository;
+    private final VenueRepository venueRepository;
+    private final IngredientInlineKeyboardService ingredientInlineKeyboardService;
+    private final AdminService adminService;
     private final TelegramBotProperties properties;
 
-    Logger logger = LoggerFactory.getLogger(PizzaSuggesterBot.class);
+    public enum BotCommand {
+        PIZZA,
+        RANDOM,
+        HELP,
+        ABOUT,
+        ADMIN,
+        CARLOS,
+        UNKNOWN
+        ;
+    }
 
-    public PizzaSuggesterBot(TelegramBotProperties properties) {
+    public PizzaSuggesterBot(TelegramBotProperties properties,
+                             CachedUserRepository cachedUserRepository,
+                             VenueRepository venueRepository,
+                             IngredientInlineKeyboardService ingredientInlineKeyboardService,
+                             AdminService adminService)
+    {
         super(new SetWebhook(properties.getWebhookUrl()), properties.getBotToken());
         this.properties = properties;
+        this.cachedUserRepository = cachedUserRepository;
+        this.venueRepository = venueRepository;
+        this.ingredientInlineKeyboardService = ingredientInlineKeyboardService;
+        this.adminService = adminService;
     }
 
     @Override
@@ -31,13 +54,88 @@ public class PizzaSuggesterBot extends SpringWebhookBot {
     }
 
     @Override
-    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        logger.debug("Got update: {}", update.toString());
-        return updateHandler.handleUpdate(update);
+    public String getBotPath() {
+        return "/callback/" + properties.getBotToken();
     }
 
     @Override
-    public String getBotPath() {
-        return "/callback/" + properties.getBotToken();
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        logger.debug("Got update: {}", update.toString());
+
+        var message = update.getMessage();
+        var callbackQuery = update.getCallbackQuery();
+        var user = cachedUserRepository.findOrAddUserByChatId(message.getChatId());
+
+        var reply = new SendMessage();
+        reply.setChatId(message.getChatId());
+        reply.setText("Unknown operation, use /help for help");
+
+        if (callbackQuery != null) {
+            logger.debug("Update type: callbackQuery");
+            // TODO handle callback
+        }
+
+        if (update.getMessage() != null) {
+            var messageText = message.getText();
+
+            if (messageText.length() > 1 && messageText.charAt(0) == '/') {
+                logger.debug("Update type: command");
+                messageText = messageText.substring(1);
+
+                var command = mapStringToBotCommand(messageText);
+                logger.debug("Mapped command string '{}' to enum type '{}'", messageText, command);
+
+                switch (command) {
+                    case PIZZA:
+                        // TODO set reply text for pizza selection
+                        reply.setText("placeholder text for /pizza command");
+                        // reply.setReplyMarkup(inlineKeyboardService.getInitialKeyboard(0L));
+                        return reply;
+
+                    case RANDOM:
+                        // TODO implement random pizza selection
+                        break;
+
+                    case HELP:
+                        // TODO set reply text for /help
+                        reply.setText("placeholder /help text");
+                        return reply;
+
+                    case ADMIN:
+                        reply.setText("Placeholder /admin text");
+                        reply.setReplyMarkup(adminService.getAdminMenu(user));
+                        return reply;
+
+                    case CARLOS:
+                        break;
+                }
+
+            }
+        }
+
+        return reply;
+    }
+
+    private BotCommand mapStringToBotCommand(String str) {
+        switch (str) {
+            case "pizza":
+                return BotCommand.PIZZA;
+
+            case "zufall":
+                return BotCommand.RANDOM;
+
+            case "start":
+            case "help":
+                return BotCommand.HELP;
+
+            case "carlos":
+                return BotCommand.CARLOS;
+
+            case "admin":
+                return BotCommand.ADMIN;
+
+            default:
+                return BotCommand.UNKNOWN;
+        }
     }
 }
