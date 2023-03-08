29703c2d5d1378c9b41a40d1d920e2353f9eeae6
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-03-08 13:59:53 +0100

    [FEATURE] add SELECTING_DIET state

--- src/main/java/eu/kaesebrot/dev/enums/UserState.java
+++ src/main/java/eu/kaesebrot/dev/enums/UserState.java
@@ -3,6 +3,9 @@ package eu.kaesebrot.dev.enums;
 public enum UserState {
     FREE,
     SELECTING_VENUE,
+    SELECTING_DIET,
+
+    // limited to non-admin users
     SENDING_ADMIN_KEY,
 
     // Limited to admin users
