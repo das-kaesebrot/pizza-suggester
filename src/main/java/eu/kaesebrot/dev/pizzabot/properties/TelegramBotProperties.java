6eb221a4352398620d59444f9ac0bf387f135df1
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-06-12 13:00:30 +0200

    [FEATURE] add supportedLocales prop, notBlank annotations

--- src/main/java/eu/kaesebrot/dev/pizzabot/properties/TelegramBotProperties.java
+++ src/main/java/eu/kaesebrot/dev/pizzabot/properties/TelegramBotProperties.java
@@ -1,14 +1,18 @@
 package eu.kaesebrot.dev.pizzabot.properties;
 
+import jakarta.validation.constraints.NotBlank;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "telegrambot")
 public class TelegramBotProperties {
+    @NotBlank
     private String botUsername;
+    @NotBlank
     private String botToken;
     private String webhookBaseUrl;
     private String primaryLocale;
+    private String[] supportedLocales;
 
     public String getBotUsername() {
         return botUsername;
@@ -40,5 +44,13 @@ public class TelegramBotProperties {
 
     public void setPrimaryLocale(String primaryLocale) {
         this.primaryLocale = primaryLocale;
+    }
+
+    public String[] getSupportedLocales() {
+        return supportedLocales;
+    }
+
+    public void setSupportedLocales(String[] supportedLocales) {
+        this.supportedLocales = supportedLocales;
     }
 }
