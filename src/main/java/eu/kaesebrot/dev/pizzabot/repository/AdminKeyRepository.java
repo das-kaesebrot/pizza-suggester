437635e727913659dcd94896c21348565e5667b6
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-06-12 20:57:26 +0200

    [FIX] annotate repositories with @Repository

--- src/main/java/eu/kaesebrot/dev/pizzabot/repository/AdminKeyRepository.java
+++ src/main/java/eu/kaesebrot/dev/pizzabot/repository/AdminKeyRepository.java
@@ -2,11 +2,11 @@ package eu.kaesebrot.dev.pizzabot.repository;
 
 import eu.kaesebrot.dev.pizzabot.model.AdminKey;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@Service
+@Repository
 public interface AdminKeyRepository extends JpaRepository<AdminKey, UUID> {
     
 }
