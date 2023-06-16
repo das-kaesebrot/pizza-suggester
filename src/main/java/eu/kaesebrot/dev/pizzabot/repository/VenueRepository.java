1ce2bcb597b685f6d5d5afe64a8a9b6a63b9d641
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-06-16 17:58:05 +0200

    [FEATURE] add findByModifiedAtGreaterThan method

--- src/main/java/eu/kaesebrot/dev/pizzabot/repository/VenueRepository.java
+++ src/main/java/eu/kaesebrot/dev/pizzabot/repository/VenueRepository.java
@@ -4,7 +4,10 @@ import eu.kaesebrot.dev.pizzabot.model.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 @Repository
 public interface VenueRepository extends JpaRepository<Venue, Long> {
-
+    List<Venue> findByModifiedAtGreaterThan(Timestamp timestamp);
 }
