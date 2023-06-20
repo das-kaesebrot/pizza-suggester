5af2f93a2227d83c26c3a2639f8e3b7b0daa04eb
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-06-20 22:11:38 +0200

    [FIX] add PizzaMenuService to CallbackHandlingServiceImpl

--- src/main/java/eu/kaesebrot/dev/pizzabot/service/CallbackHandlingServiceImpl.java
+++ src/main/java/eu/kaesebrot/dev/pizzabot/service/CallbackHandlingServiceImpl.java
@@ -4,6 +4,7 @@ import eu.kaesebrot.dev.pizzabot.bot.PizzaSuggesterBot;
 import eu.kaesebrot.dev.pizzabot.model.CachedUser;
 import eu.kaesebrot.dev.pizzabot.service.menu.AdminMenuService;
 import eu.kaesebrot.dev.pizzabot.service.menu.MenuService;
+import eu.kaesebrot.dev.pizzabot.service.menu.PizzaMenuService;
 import eu.kaesebrot.dev.pizzabot.service.menu.UserMenuService;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
@@ -15,10 +16,12 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class CallbackHandlingServiceImpl implements CallbackHandlingService {
     private final AdminMenuService adminMenuService;
     private final UserMenuService userMenuService;
+    private final PizzaMenuService pizzaMenuService;
 
-    public CallbackHandlingServiceImpl(AdminMenuService adminMenuService, UserMenuService userMenuService) {
+    public CallbackHandlingServiceImpl(AdminMenuService adminMenuService, UserMenuService userMenuService, PizzaMenuService pizzaMenuService) {
         this.adminMenuService = adminMenuService;
         this.userMenuService = userMenuService;
+        this.pizzaMenuService = pizzaMenuService;
     }
 
     @Override
@@ -27,10 +30,12 @@ public class CallbackHandlingServiceImpl implements CallbackHandlingService {
 
         MenuService handlingService;
 
-        if (query.getData().startsWith(adminMenuService.CALLBACK_PREFIX)) {
+        if (query.getData().startsWith(adminMenuService.getCallbackPrefix())) {
             handlingService = adminMenuService;
-        } else if (query.getData().startsWith(userMenuService.CALLBACK_PREFIX)) {
+        } else if (query.getData().startsWith(userMenuService.getCallbackPrefix())) {
             handlingService = userMenuService;
+        } else if (query.getData().startsWith(pizzaMenuService.getCallbackPrefix())) {
+            handlingService = pizzaMenuService;
         } else {
             throw new RuntimeException(String.format("Can't map callback data to service: %s", query.getData()));
         }
