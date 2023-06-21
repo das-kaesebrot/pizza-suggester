2114669e8f21a65218940bc603333fd946e2a230
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-06-21 16:38:15 +0200

    [FEATURE] allow setting coordinates by string with N/S W/E prefix/suffix

--- src/main/java/eu/kaesebrot/dev/pizzabot/model/VenueInfo.java
+++ src/main/java/eu/kaesebrot/dev/pizzabot/model/VenueInfo.java
@@ -61,6 +61,25 @@ public class VenueInfo implements Serializable {
     public Double getLongitude() {
         return longitude;
     }
+    public String getCoordinatesString() {
+        var string = new StringBuilder();
+
+        if (latitude >= 0.0)
+            string.append("N");
+        else
+            string.append("S");
+
+        string.append(latitude).append(", ");
+
+        if (longitude < 0.0)
+            string.append("W");
+        else
+            string.append("E");
+
+        string.append(longitude);
+
+        return string.toString();
+    }
 
     public String getPhoneNumber() {
         return phoneNumber;
@@ -87,6 +106,46 @@ public class VenueInfo implements Serializable {
     }
     public void setLongitude(Double longitude) {
         this.longitude = longitude;
+    }
+
+    public void setCoordinatesByString(String coordinatesString) {
+        var latLong = coordinatesString
+                .replaceAll(" ", "")
+                .split(",");
+
+        if (latLong.length != 2)
+            throw new IllegalArgumentException(String.format("Coordinates provided aren't in the right format! Given data: %s", coordinatesString));
+
+        String REGEX_LATITUDE_NORTH = "^(?i)([N]\\d+.\\d+)|(\\d+.\\d+[N])$";
+        String REGEX_LATITUDE_SOUTH = "^(?i)([S]\\d+.\\d+)|(\\d+.\\d+[S])$";
+
+        String REGEX_LONGITUDE_WEST = "^(?i)([W]\\d+.\\d+)|(\\d+.\\d+[W])$";
+        String REGEX_LONGITUDE_EAST = "^(?i)([E]\\d+.\\d+)|(\\d+.\\d+[E])$";
+
+        var latitudeString = latLong[0];
+        var longitudeString = latLong[1];
+
+        if (latitudeString.matches(REGEX_LATITUDE_NORTH)
+                || latitudeString.matches(REGEX_LATITUDE_SOUTH)) {
+
+            if (latitudeString.matches(REGEX_LATITUDE_SOUTH))
+                latitudeString = "-" + latitudeString;
+
+            latitudeString = latitudeString.replaceAll("(?i)[NS]", "");
+        }
+
+        this.setLatitude(Double.parseDouble(latitudeString));
+
+        if (longitudeString.matches(REGEX_LONGITUDE_WEST)
+                || longitudeString.matches(REGEX_LONGITUDE_EAST)) {
+
+            if (longitudeString.matches(REGEX_LONGITUDE_EAST))
+                longitudeString = "-" + longitudeString;
+
+            longitudeString = longitudeString.replaceAll("(?i)[WE]", "");
+        }
+
+        this.setLongitude(Double.parseDouble(longitudeString));
     }
 
     public void setPhoneNumber(String phoneNumber) {
