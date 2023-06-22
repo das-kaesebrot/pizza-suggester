8e0cdb09b083f23878c4cbe21c8bc47ef65d4da4
das-kaesebrot <49565683+das-kaesebrot@users.noreply.github.com>
2023-06-22 21:03:32 +0200

    [FIX] annotate deletePizzasByVenue method as transactional

--- src/main/java/eu/kaesebrot/dev/pizzabot/repository/PizzaRepository.java
+++ src/main/java/eu/kaesebrot/dev/pizzabot/repository/PizzaRepository.java
@@ -3,16 +3,17 @@ package eu.kaesebrot.dev.pizzabot.repository;
 import eu.kaesebrot.dev.pizzabot.enums.UserDiet;
 import eu.kaesebrot.dev.pizzabot.model.Pizza;
 import eu.kaesebrot.dev.pizzabot.model.Venue;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
     List<Pizza> findByVenue(Venue venue);
     List<Pizza> findByVenueAndMinimumUserDietGreaterThanEqual(Venue venue, UserDiet diet);
+    @Transactional
     void deletePizzasByVenue(Venue venue);
     boolean existsPizzasByVenue(Venue venue);
 }
